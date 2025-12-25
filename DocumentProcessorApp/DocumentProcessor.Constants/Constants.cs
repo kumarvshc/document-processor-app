7f7dfa65d3ed4de6d62109065d9fa23213dc96cf
Veerabadran, Kumar <kumar.veerabadran@sogeti.com>
2025-12-25 22:22:26 +0100

    Changed the Azure service bus queue name

--- DocumentProcessorApp/DocumentProcessor.Constants/Constants.cs
+++ DocumentProcessorApp/DocumentProcessor.Constants/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string CONST_KEYSCAN_QUEUE_NAME = "keyscan-documents";
-        public const string CONST_PATTERN_EXTRACT_QUEUE_NAME = "pattern-extract-documents";
+        public const string CONST_KEYSCAN_QUEUE_NAME = "scan-documents";
+        public const string CONST_PATTERN_EXTRACT_QUEUE_NAME = "pattern-documents";
     }
 }
