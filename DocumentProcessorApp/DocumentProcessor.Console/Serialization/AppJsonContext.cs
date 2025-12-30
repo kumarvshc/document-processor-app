70f23dafcd09824826fd2d6978c6837128fbfda2
Kumar Veerabadran <v.kumarshc@yahoo.co.in>
2025-12-30 13:42:11 +0100

    Update AppJsonContext.cs

--- DocumentProcessorApp/DocumentProcessor.Console/Serialization/AppJsonContext.cs
+++ DocumentProcessorApp/DocumentProcessor.Console/Serialization/AppJsonContext.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace DocumentProcessor.Console.Serialization
 {
-
     [JsonSourceGenerationOptions(
         PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
         DefaultIgnoreCondition = JsonIgnoreCondition.Never)]
@@ -11,5 +10,4 @@ namespace DocumentProcessor.Console.Serialization
     internal partial class AppJsonContext : JsonSerializerContext
     {
     }
-
 }
